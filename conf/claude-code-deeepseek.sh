42b092f16adecc9a12832436c437b2eeec298f5a
zhaopan <syszhaopan@gmail.com>
2026-05-18 18:17:32 +0800

    fix

---
+++ conf/claude-code-deeepseek.sh
@@ -0,0 +1,8 @@
+export ANTHROPIC_AUTH_TOKEN=
+export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
+export ANTHROPIC_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_EFFORT_LEVEL=max
